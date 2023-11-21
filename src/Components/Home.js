a87f0fb8eefd5282d5f7f636375aecb6f144751b
Ridhichauhan <112615560+Ridhichauhan@users.noreply.github.com>
2023-11-21 15:32:39 +0530

    done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,17 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <div className=''>
+      <div className=' bg-navBar h-[5vh] w-screen'>
+      <div className='flex justify-between '>
+      <div className='flex space-x-2  text-textWhite ml-[8vw] mt-[1vh]'>
+        <h1 className=''>(414)857 -0107</h1>
+        <h1 className=''>
+        yummy@bistrobliss</h1>
+      </div>
+      <div>
 
+      </div>
+      </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xxs: "250px",
+        xs: "375px",
+        xs2: "500px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1920px",
+        "4xl": "2560px",
+        "5xl": "3840px",
+      },
+      colors: {
+        navBar:"#474747",
+        textWhite:"#ffffff"
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [],
 }
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   theme: {
+//     screens: {
+//       'xxs':'250px',
+//       'xs':'375px',
+//       'sm': '640px',
+//       'md': '768px',
+//       'lg': '1024px',
+//       'xl': '1280px',
+//       '2xl': '1536px',
+//     }
+//   }
+// }

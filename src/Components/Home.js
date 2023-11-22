a50681293eb5d917936405d105b7306c5eb4bbb4
Ridhichauhan <112615560+Ridhichauhan@users.noreply.github.com>
2023-11-22 18:46:18 +0530

    done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,30 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { IoCallOutline } from "react-icons/io5";
 import { AiOutlineMail } from "react-icons/ai";
 import { IoIosMenu } from "react-icons/io";
 
 const Home = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
-
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
-
   return (
     <div>
       <div className="">
         <div className="hidden sm:flex justify-between bg-navBar h-[6vh] w-screen">
           <div className="flex space-x-6  text-textWhite ml-[8vw] mt-[1vh]">
             <h1 className="flex space-x-2 mt-2">
-              {" "}
-              <IoCallOutline className="mt-1 mr-2" /> (414)857-0107
+              <IoCallOutline className="mt-1 mr-2" />
+              (414)857-0107
             </h1>
             <h1 className="flex space-x-2 mt-1">
-              <AiOutlineMail className="mt-2 mr-2"/>
+              <AiOutlineMail className="mt-2 mr-2" />
               yummy@bistrobliss
             </h1>
           </div>
@@ -35,7 +25,10 @@ const Home = () => {
             <img src="./fb.png" alt="" className="h-8 w-8"></img>
             <img src="./git.png" alt="" className="h-8 w-8"></img>
           </div>
-        
+        </div>
+
+        <div className="xs:block xxs:block hidden sm:hidden lg:hidden md:hidden xl:hidden bg-navBar h-[6vh]">
+          <IoIosMenu color="white" className="flex " />
         </div>
       </div>
     </div>

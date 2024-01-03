f6c1f369e607abadf746ed2ccaa6110c5dfba81d
Ridhichauhan <112615560+Ridhichauhan@users.noreply.github.com>
2024-01-03 16:53:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+        <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/menu" element={<Menu />} />
